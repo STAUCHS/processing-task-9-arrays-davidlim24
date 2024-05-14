4917e4a78fce7c806d584f7ea52c7f8cc49f37b9
davidlim24 <david.lim25@ycdsbk12.ca>
2024-05-14 14:17:59 -0400

    please work on

--- Sketch.java
+++ Sketch.java
@@ -2,17 +2,42 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
+  // related arrays fr the (x, y) coordinate of the snow ball
+  float[] snowX = new float[42];
+  float[] snowY = new float[42];
+  int snowDiameter = 10;
+
   public void settings() {
     size(400, 400);
   }
 
-  public void setup() {
+    public void setup() {
     background(0);
+    for (int i = 0; i < snowX.length; i++) {
+      snowX[i] = random(width);
+      snowY[i] = random(height);
+    }
   }
+  
 
   public void draw() {
-    
+   background(0);
+  
+   // Draw snow
+  snow();
   }
   
   // All other defined methods are written below:
-}
+  public void snow() {
+    for (int i = 0; i < snowX.length; i++) {
+      circle(snowX[i], snowY[i], snowDiameter);
+    
+      //reset snowflakes
+      snowY[i] += 2;
+      if (snowY[i] > height){
+        snowY[i] = 0;
+          }
+        }
+      }
+    } 
+
